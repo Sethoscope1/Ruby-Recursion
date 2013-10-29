2efd1d6e31b8483776123095cc022c4c8d156793
Sethoscope1 <sethleland@gmail.com>
2013-10-29 15:03:14 -0400

    Adds deep dup, fibonacci, binary search

--- recursion.rb
+++ recursion.rb
@@ -38,13 +38,51 @@ end
 
 # Recursive array deep dup
 
-def rec_deep_dup(array)
-
-  array.each do |element|
-    if element.is_a?(array)
-
-    else
-
+def rec_deep_dup(input)
+  if !input.is_a?(Array)
+    input
+  else
+    result = []
+    input.each do |element|
+      result << rec_deep_dup(element)
     end
   end
 end
+
+
+# Recursive Fibonacci
+
+def rec_fibs(num)
+  case num
+  when 1
+    [0]
+  when 2
+    [0, 1]
+  else
+    first_fibs = rec_fibs(num - 1)
+    first_fibs << first_fibs[-1] + first_fibs[-2]
+  end
+end
+
+puts rec_fibs(16)
+
+
+# Recursive binary search (sorted array, target number)
+def rec_bin_search(array, target)
+  return nil if array.length == 0
+
+  midpoint = array.length / 2
+
+  return midpoint if array[midpoint] == target
+
+  if target < array[midpoint]
+    rec_bin_search(array.take(midpoint), target)
+  else
+    top = rec_bin_search(array.drop(midpoint + 1), target)
+    top == nil ? nil : top + (midpoint + 1)
+  end
+end
+
+
+
+
